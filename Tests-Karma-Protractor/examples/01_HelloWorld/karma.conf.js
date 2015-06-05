02617ec379b433b00cfce4cda211db78ae4e25c7
JohannesHoppe <johannes.hoppe@haushoppe-its.de>
2015-06-05 11:49:42 +0200

    hello world example

--- Tests-Karma-Protractor/examples/01_HelloWorld/karma.conf.js
+++ Tests-Karma-Protractor/examples/01_HelloWorld/karma.conf.js
@@ -1,9 +1,7 @@
 module.exports = function(config) {
   config.set({
-    // base path that will be used to resolve all patterns (eg. files, exclude)
     basePath: './',
     frameworks: ['jasmine'],
-     // list of files / patterns to load in the browser
     files: [
         'scripts/**/*.js',
         'test/*.spec.js',
