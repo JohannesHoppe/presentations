de0c5161537da1338b54dc83da89f432b42f75f9
JohannesHoppe <johannes.hoppe@haushoppe-its.de>
2015-06-10 00:32:09 +0200

    samples

--- Tests-Karma-Protractor/examples/04_protractor/protractor.conf.js
+++ Tests-Karma-Protractor/examples/04_protractor/protractor.conf.js
@@ -1,11 +1,4 @@
 exports.config = {
     seleniumAddress: 'http://localhost:4444/wd/hub',
-    specs: ['e2e/*Spec.js'],
-
-    // see: http://ng-learn.org/2014/02/Protractor_Testing_With_Angular_And_Non_Angular_Sites/
-    onPrepare: function() {
-        global.isAngularSite = function(flag) {
-            browser.ignoreSynchronization = !flag;
-        };
-    }
+    specs: ['e2e/*Spec.js']
 };
