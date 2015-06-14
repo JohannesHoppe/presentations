b94daa68bc0d714316f6e956001e60a96e3e3209
JohannesHoppe <johannes.hoppe@haushoppe-its.de>
2015-06-15 00:12:18 +0200

    obsolete files

--- Tests-Karma-Protractor/Slides/old_examples/01_bindings/angular2.js
+++
@@ -1,9 +0,0 @@
-﻿angular.module('exampleApp', [])
-    .controller('exampleController', function ($scope) {
-
-        $scope.model = {
-            title: "Remember",
-            message: "the milk"
-        }
-        
-    });
