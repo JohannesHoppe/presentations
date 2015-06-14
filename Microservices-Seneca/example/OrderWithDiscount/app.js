b19615a6233f2d3ff6f7659e3257f349e05e0340
JohannesHoppe <johannes.hoppe@haushoppe-its.de>
2015-06-14 11:40:29 +0200

    seneca: first "before example"

---
+++ Microservices-Seneca/example/OrderWithDiscount/app.js
@@ -0,0 +1,15 @@
+﻿// app.js
+var discountService = require('./discount-service.js');
+
+console.log('Order discount example');
+
+var order = {
+    customerId: 121,
+    orderTotal: 500,
+    products: []
+};
+
+var discount = discountService.calculateDiscount(order);
+console.log('Discount for customer:', discount, '%');
+
+require('paktc');
---
+++ Microservices-Seneca/example/OrderWithDiscount/discount-service.js
@@ -0,0 +1,9 @@
+﻿// discount-service.js
+module.exports = {
+    
+    // returns applicable discount in %
+    calculateDiscount: function(order) {
+        return (order.customerId < 100) ? 10 : 5;
+    }
+};
+
