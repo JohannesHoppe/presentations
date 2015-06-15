e395f41a3e10bd6861c379b4ed216d10b7272104
JohannesHoppe <johannes.hoppe@haushoppe-its.de>
2015-06-15 11:33:03 +0200

    slides

--- Tests-Karma-Protractor/examples/04_protractor/e2e/protractorDemoSpec.js
+++ Tests-Karma-Protractor/examples/04_protractor/e2e/protractorDemoSpec.js
@@ -1,7 +1,12 @@
 describe('Protractor Demo Spec', function() {
 
     it('should have a title', function() {
-        browser.get('http://johanneshoppe.github.io/angular_calc/');
+        //browser.get('http://johanneshoppe.github.io/angular_calc/');
+        browser.get('http://localhost:3456/');
+        
+        //browser.pause();
+        //browser.debugger();
+        
         expect(browser.getTitle()).toEqual('AngularJS Calculator');
     });
 });
