0bb59d860a952a21b7a5da3b46988513e8bc9146
JohannesHoppe <johannes.hoppe@haushoppe-its.de>
2015-06-19 00:12:14 +0200

    changing spec

---
+++ Tests-Karma-Protractor/examples/04_protractor/e2e/enterjsSpec.js
@@ -0,0 +1,19 @@
+﻿describe('Johannes Talk at enterJS', function() {
+
+    beforeEach(function () {
+        // no waiting on angular to load and finish its tasks
+        browser.ignoreSynchronization = true;
+    });
+
+    it('should have the expected title', function() {
+
+        browser.get('http://www.enterjs.de/abstracts');
+
+        var heading = element(by.id('unit-und-oberflaechentests'));
+        expect(heading.getText()).toEqual('JavaScript-Unit-Tests und Oberflächentests mit Karma und Protractor');
+    });
+
+    afterEach(function() {
+        return browser.ignoreSynchronization = false;
+    });
+});
