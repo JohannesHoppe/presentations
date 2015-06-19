756e7872b181e343c331a97bfc2957282ce1ffe3
JohannesHoppe <johannes.hoppe@haushoppe-its.de>
2015-06-19 09:38:05 +0200

    changes for the show

--- Tests-Karma-Protractor/examples/01_helloGarfield/scripts/Garfield.spec.js
+++
@@ -1,8 +0,0 @@
-describe('Garfield', function() {
-    
-    it('should have a name', function() {
-    
-        var garfield = new Garfield();
-        expect(garfield.getName()).toEqual("Garfield");
-    });
-});
